9a5a4caba6a731f55ff94fec3c2f9c045801924c
Kaique Ferreira <kkaiquefs26@gmail.com>
2023-06-15 09:18:14 -0300

    Início controle de versão

---
+++ src/main/java/com/itb/inf2bm/comercio/model/Produto.java
@@ -0,0 +1,15 @@
+package com.itb.inf2bm.comercio.model;
+
+public class Produto {
+
+	private Long id;
+	private String nome;
+	private String descricao;
+	private String codigoBarras;
+	private double preco;
+	
+	// public: Acesso liberado para todas as classes.
+	// private: Acesso liberado apenas para os membros da própria classe.
+
+	
+}
